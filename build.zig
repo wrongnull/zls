6f2bdf657014be84a29b46106f82df25317591dd
Techatrix <19954306+Techatrix@users.noreply.github.com>
2024-04-09 00:14:06 +0200

    temporarily reduce build time of deployment

--- build.zig
+++ build.zig
@@ -116,14 +116,14 @@ pub fn build(b: *Build) !void {
     });
 
     const targets: []const std.Target.Query = &.{
-        .{ .cpu_arch = .x86_64, .os_tag = .windows },
+        // .{ .cpu_arch = .x86_64, .os_tag = .windows },
         .{ .cpu_arch = .x86_64, .os_tag = .linux },
-        .{ .cpu_arch = .x86_64, .os_tag = .macos },
-        .{ .cpu_arch = .x86, .os_tag = .windows },
-        .{ .cpu_arch = .x86, .os_tag = .linux },
-        .{ .cpu_arch = .aarch64, .os_tag = .linux },
-        .{ .cpu_arch = .aarch64, .os_tag = .macos },
-        .{ .cpu_arch = .wasm32, .os_tag = .wasi },
+        // .{ .cpu_arch = .x86_64, .os_tag = .macos },
+        // .{ .cpu_arch = .x86, .os_tag = .windows },
+        // .{ .cpu_arch = .x86, .os_tag = .linux },
+        // .{ .cpu_arch = .aarch64, .os_tag = .linux },
+        // .{ .cpu_arch = .aarch64, .os_tag = .macos },
+        // .{ .cpu_arch = .wasm32, .os_tag = .wasi },
     };
 
     const release_step = b.step("release", "Build all release binaries");
